9ccf75f4ef110ae26e0f8e902fadfb65e891f87d
Shreyash Kumar <shreyash.2101142ec@iiitbh.ac.in>
2024-12-09 14:35:10 +0530

    feat: updated with API Documentation and future improvements

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from app.api.endpoints import router
+import os
 
 app = FastAPI()
 
 app.include_router(router)
 
+
+INDEX_FILE_PATH = os.path.join(os.path.dirname(__file__), "static", "index.html")
 @app.get("/")
 async def root():
-    return "Hello, World!"
+    return FileResponse(INDEX_FILE_PATH)
